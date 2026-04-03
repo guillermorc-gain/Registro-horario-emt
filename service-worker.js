cafb9837b206c798374f6682395279ac7bf7c7b7
guillermorc-gain <guillermo.rc82@gmail.com>
2026-04-03 12:58:50 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,44 @@
-const CACHE_NAME = 'horas-v1';
+const CACHE_NAME = 'horas-v2';
+const BASE_PATH = '/'; // ajusta si no estás en raíz del dominio
+
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/service-worker.js'
+  BASE_PATH,
+  BASE_PATH + 'index.html',
+  BASE_PATH + 'manifest.json',
+  BASE_PATH + 'icons/icon-192.png',
+  BASE_PATH + 'icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache).catch(() => {});
-    })
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(urlsToCache).catch(() => {})
+    )
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
+    caches.keys().then(cacheNames =>
+      Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
-      );
-    })
+      )
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
+  const request = event.request;
+
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-      .catch(() => caches.match('/index.html'))
+    caches.match(request)
+      .then(response => response || fetch(request))
+      .catch(() => caches.match(BASE_PATH + 'index.html'))
   );
 });
